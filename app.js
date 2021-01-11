93e5294af5daf3a132c071a74766bbfadddc0711
haydensprinkle <haydensprinkle@gmail.com>
2021-01-10 22:55:48 -0800

    tried and failed to fix issue, attempting again tomorrow

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const tweetText = document.getElementById("tweet-text").value;
 const postButton = document.getElementById("post");
-let postTweet = (event) => {
-  event.preventDefault();
+function postTweet(e) {
+  e.preventDefault(e);
   console.log(tweetText);
-};
+}
+postButton.addEventListener("click", postTweet());
+
+//cannot figure out what's wrong !!!
